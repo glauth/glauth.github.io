f1f93cbcd5d22254ee9e77afcd2a6284ae8c8d88
Chris F Ravenscroft <chris@voilaweb.com>
2023-11-11 17:31:58 -0800

    chore: update top level packages

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,64 +1,64 @@
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: {
-    enabled: true,
-    mode: 'all',
-    content: ["./*.html", "docs/**/*.html", "comparison/*.html"],
-    options: {
-      safelist: [],
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
     },
-  },
-  darkMode: 'class',
-  theme: {
-    container: {
-      center: true,
-    },
-    extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            maxWidth: '100%',
-            '.prose a.edit, .tag a': {
-              color: '#333',
-              'text-decoration': 'none',
-            },
-            'ul.footer-nav': {
-              '::before': {
-                display: 'none',
-                'text-decoration': 'none',
-              }
-            },
-            'ul.contains-task-list': {
-              '::before': {
-                display: 'none',
-              }
-            },
-            'ul.spacelog': {
-              '::before': {
-                display: 'none',
-              }
-            },
-          },
+    purge: {
+        enabled: true,
+        mode: 'all',
+        content: ["./*.html", "docs/**/*.html", "comparison/*.html", "planet/*.html"],
+        options: {
+            safelist: [],
         },
-      }
     },
-  }, 
-  variants: {},
-  plugins: [
-    require('tailwind-content-placeholder')({
-      placeholders: {
-        'paragraph': {
-          height: 4, // em
-          rows: [[100], [100], [40], [],], // %
+    darkMode: 'class',
+    theme: {
+        container: {
+            center: true,
         },
-      },
-    }),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-  ],
+        extend: {
+            typography: {
+                DEFAULT: {
+                    css: {
+                        maxWidth: '100%',
+                        '.prose a.edit, .tag a': {
+                            color: '#333',
+                            'text-decoration': 'none',
+                        },
+                        'ul.footer-nav': {
+                            '::before': {
+                                display: 'none',
+                                'text-decoration': 'none',
+                            }
+                        },
+                        'ul.contains-task-list': {
+                            '::before': {
+                                display: 'none',
+                            }
+                        },
+                        'ul.spacelog': {
+                            '::before': {
+                                display: 'none',
+                            }
+                        },
+                    },
+                },
+            }
+        },
+    },
+    variants: {},
+    plugins: [
+        require('tailwind-content-placeholder')({
+            placeholders: {
+                'paragraph': {
+                    height: 4, // em
+                    rows: [[100], [100], [40], [],], // %
+                },
+            },
+        }),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+    ],
 }
--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,16 @@
 const cssnano = require('cssnano')({
-  preset: ['default', {
-    discardComments: {
-      removeAll: true,
-    },
-  }]
+    preset: ['default', {
+        discardComments: {
+            removeAll: true,
+        },
+    }]
 })
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require('tailwindcss')('assets/tailwind.config.js'),
-    ...process.env.NODE_ENV === 'production' ? [cssnano] : [],
-    require('autoprefixer'),
-  ],
+    plugins: [
+        require("postcss-import"),
+        require('tailwindcss/nesting')(require('postcss-nesting')),
+        require('tailwindcss')('assets/tailwind.config.js'),
+        ...process.env.NODE_ENV === 'production' ? [cssnano] : [],
+        require('autoprefixer'),
+    ],
 };
